a2049d7b39d7f1475566b9211eec43b5fb95b336
John Boctor <jboctor@rivs.com>
2019-10-09 15:13:36 -0500

    Added gulp and got header and footer working

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var csso = require('gulp-csso');
+var concat = require('gulp-concat');
+ 
+var scripts = [
+	'vendor/components/jquery/jquery.js',
+	'vendor/twbs/bootstrap/dist/js/bootstrap.js'
+];
+
+var styles = [
+	'vendor/twbs/bootstrap/dist/css/bootstrap.css'
+];
+
+gulp.task('pack-js', function () {    
+    return gulp.src(scripts)
+		.pipe(concat('script.js'))
+		.pipe(uglify({ mangle: false }))
+        .pipe(gulp.dest('public/build/js'));
+});
+ 
+gulp.task('pack-css', function () {    
+    return gulp.src(styles)
+		.pipe(concat('stylesheet.css'))
+		.pipe(csso())
+        .pipe(gulp.dest('public/build/css'));
+});
+ 
+gulp.task('default', gulp.parallel('pack-js', 'pack-css'));
